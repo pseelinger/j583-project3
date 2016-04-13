3938055615e50efd2482f3a25443981d4522a6c3
Patrick Seelinger <Patrick Seelinger>
2016-04-12 21:20:16 -0400

    working on making line graph data change on selection

--- js/line.js
+++ js/line.js
@@ -16,21 +16,40 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-var totalLine = d3.svg.line()
+//Initial line will show Alaska's data
+var lineInit = d3.svg.line()
     .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.Connecticut); })
+    .y(function(d) { return y(d.Alaska); })
     .interpolate("linear");
-
+var lines= [];
+var states= [];
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//Dropdown Menu
+var select_state = d3.select("body").append("select")
+    .attr("class", "select_state");
 
+//Get the data
 d3.csv('states.csv', function(data) {
-
+//Make a list of states in the dropdown menu and a line for each state
+    for(var i = 1; i <= Object.keys(data[0]).length - 1; i++ ){
+      states[i-1] = Object.keys(data[0])[i];
+    }
+    for(var i = 1; i <= Object.keys(data[0]).length - 1; i++ ){
+      select_state.append("option").text(Object.keys(data[0])[i]);
+      
+      console.log(states[i-1]);
+      lines[i-1] = d3.svg.line()
+          .x(function(d) { return x(d["year"]); })
+          .y(function(d) { var currentState = String(states[i-1]); return y(d[currentState]); })
+          .interpolate("linear");
+    }
+//Scale data to fit in graph
   x.domain(d3.extent(data, function(d) { return d.year; }));
-  y.domain([0, 100]);
+  y.domain([0,100]);
 
   svg.append("g")
       .attr("class", "x axis")
@@ -44,11 +63,14 @@ d3.csv('states.csv', function(data) {
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
-      .style("text-anchor", "end");
+      .style("text-anchor", "end")
+      .text("Percent");
 
-  svg.append("path")
+var drawline = function(line){ svg.append("path")
       .datum(data)
       .attr("class", "line")
-      .attr("d", totalLine);
+      .attr("d", line);
+};
+drawline(lines[4]);
 
 });
